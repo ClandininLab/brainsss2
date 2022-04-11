eacd0737f026ab00d6dd87b23ab5fd8581e56c7b
Russ Poldrack <poldrack@gmail.com>
2022-04-11 13:03:04 -0700

    working version, full refactor - still to clean up logging

--- scripts/bleaching_qc.py
+++ scripts/bleaching_qc.py
@@ -5,38 +5,39 @@ import json
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_triangle
 import psutil
-import brainsss
+#import brainsss
 import nibabel as nib
+import argparse
+from pathlib import Path
 
-def main(args):
+def parse_args(input):
+    parser = argparse.ArgumentParser(description='run bleaching qc')
+    parser.add_argument('-d', '--dir', type=str, 
+        help='directory containing func or anat data', required=True)
+    parser.add_argument('--verbose', action='store_true', help='verbose output')
+    args = parser.parse_args(input)
+    return(args)
 
-    logfile = args['logfile']
-    directory = args['directory'] # directory will be a full path to either an anat/imaging folder or a func/imaging folder
-    dirtype = args['dirtype']
-    width = 120
-    printlog = getattr(brainsss.Printlog(logfile=logfile), 'print_to_log')
 
-    #################
-    ### Load Data ###
-    #################
-
-    if dirtype == 'func':
-        files = ['functional_channel_1', 'functional_channel_2']
-    elif dirtype == 'anat':
-        files = ['anatomy_channel_1', 'anatomy_channel_2']
+def load_data(args):
+    """determine directory type and load data"""
+    files = [f.as_posix() for f in Path(args.dir).glob('*_channel*.nii')]
     data_mean = {}
     for file in files:
-        full_file = os.path.join(directory, file + '.nii')
-        if os.path.exists(full_file):
-            brain = np.asarray(nib.load(full_file).get_data(), dtype='uint16')
+        if args.verbose:
+            print(f'processing {file}')
+        if os.path.exists(file):
+            brain = np.asarray(nib.load(file).get_fdata(), dtype='uint16')
             data_mean[file] = np.mean(brain,axis=(0,1,2))
+            del brain
         else:
-            printlog(F"Not found (skipping){file:.>{width-20}}")
+            print(F"Not found (skipping){file:.>{width-20}}")
+    return(data_mean)
 
-    ##############################
-    ### Output Bleaching Curve ###
-    ##############################
 
+def get_bleaching_curve(data_mean, args):
+    """get bleaching curve"""
+    
     plt.rcParams.update({'font.size': 24})
     fig = plt.figure(figsize=(10,10))
     signal_loss = {}
@@ -55,14 +56,21 @@ def main(args):
     for file in data_mean:
         loss_string = loss_string + file + ' lost' + F'{int(signal_loss[file])}' +'%\n'
     plt.title(loss_string, ha='center', va='bottom')
-    # plt.text(0.5,0.9,
-    #          loss_string,
-    #          horizontalalignment='center',
-    #          verticalalignment='center',
-    #          transform=plt.gca().transAxes)
 
-    save_file = os.path.join(directory, 'bleaching.png')
+    save_file = os.path.join(args.dir, 'bleaching.png')
     plt.savefig(save_file,dpi=300,bbox_inches='tight')
 
+
+
 if __name__ == '__main__':
-    main(json.loads(sys.argv[1]))
+    args = parse_args(sys.argv[1:])
+
+    #TODO: Fix logging
+    # logfile = args['logfile']
+    width = 120
+
+    print('loading data')
+    data_mean = load_data(args)
+
+    print('getting bleaching curve')
+    get_bleaching_curve(data_mean, args)
