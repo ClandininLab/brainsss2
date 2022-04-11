6f025d87ce235a13ba382bcd8d20fb98c7f4ac1e
Russ Poldrack <poldrack@gmail.com>
2022-04-11 13:07:07 -0700

    finalizing before removal

--- scripts/ants_moco_test.py
+++ scripts/ants_moco_test.py
@@ -4,37 +4,71 @@ import ants
 import os 
 import nibabel as nib
 import numpy as np
+from brainsss.utils import get_resolution
+import pickle
+from ants_utils import get_dataset_resolution, get_motion_parameters_from_transforms
 
-basedir = '/data/brainsss/processed/fly_001/func_0/imaging'
 
-print('reading full img')
-fullimg = nib.load(os.path.join(basedir, 'functional_channel_1.nii'))
+if __name__ == "__main__":
+    basedir = '/data/brainsss/processed/fly_001/func_0/imaging'
 
-fullimg_data = fullimg.get_fdata(dtype='float32')
+    print('reading full img')
+    fullimg = nib.load(os.path.join(basedir, 'functional_channel_1.nii'))
 
-start = 0
-end = 100
+    fullimg_data = fullimg.get_fdata(dtype='float32')
 
-partimg_data = fullimg_data[:, :, :, start:end]
+    start = 0
+    end = 500
 
-meanimg = ants.image_read(os.path.join(basedir, 'functional_channel_1_mean.nii'))
+    partimg_data = fullimg_data[:, :, :, start:end]
 
-partimg_ants = ants.from_numpy(partimg_data)
+    meanimg = ants.from_numpy(np.mean(fullimg_data, axis=3))
 
-print('running moco')
-transform_type = 'Rigid' # 'SyN'
-mytx = ants.motion_correction(image=partimg_ants, fixed=meanimg,
-    verbose=True, type_of_transform=transform_type, total_sigma=5, flow_sigma=5)
+    partimg_ants = ants.from_numpy(partimg_data)
 
-mytx['motion_corrected'].to_filename(os.path.join(basedir, 'motion_corrected_ants_part1.nii'))
+    print('running moco')
+    transform_type =  'SyN' # 'Rigid' #
 
-print('creating warped images')
-warped_img = np.zeros(partimg_data.shape)
+    use_ants_moco = True
+    total_sigma=0
+    flow_sigma=3
 
-for i in range(partimg_data.shape[3]):
-    tmpimg = ants.from_numpy(partimg_data[:, :, :, i])
-    warped = ants.apply_transforms(fixed=meanimg, moving=tmpimg, 
-        transformlist=mytx['motion_parameters'][i])
-    warped_img[:, :, :, i] = warped[:, :, :]
+    warped_img = np.zeros(partimg_data.shape)
 
-ants.from_numpy(warped_img).to_filename(os.path.join(basedir, 'moco_manual_part1.nii'))
+    mk_warped = True 
+
+    if use_ants_moco:
+
+        mytx = ants.motion_correction(image=partimg_ants, fixed=meanimg,
+            verbose=True, type_of_transform=transform_type, 
+            total_sigma=total_sigma, flow_sigma=flow_sigma)
+        analysis_string = f'transform-{transform_type}_totalsigma-{total_sigma}_flowsigma-{flow_sigma}_start-{start}_end-{end}'
+        mytx['motion_corrected'].to_filename(os.path.join(basedir, f'moco_{analysis_string}.nii'))
+
+        transform_parameters = get_motion_parameters_from_transforms(mytx['motion_parameters'],
+            get_dataset_resolution(basedir))
+
+        
+        if mk_warped:
+            print('creating warped images')
+
+            for i in range(partimg_data.shape[3]):
+                tmpimg = ants.from_numpy(partimg_data[:, :, :, i])
+                warped = ants.apply_transforms(fixed=meanimg, moving=tmpimg, 
+                    transformlist=mytx['motion_parameters'][i])
+                warped_img[:, :, :, i] = warped[:, :, :]
+
+    else:
+        # use standard ants registration
+        print('registering and creating warped images')
+
+        for i in range(partimg_data.shape[3]):
+            tmpimg = ants.from_numpy(partimg_data[:, :, :, i])
+            mytx = ants.registration(fixed=meanimg, moving=tmpimg,
+                type_of_transform=transform_type, 
+                total_sigma=total_sigma, flow_sigma=flow_sigma)
+            mytx['warpedmovout'].to_filename(os.path.join(basedir, f'moco_manual_{transform_type}_idx{i}.nii'))
+            warped_img[:, :, :, i] = mytx['warpedmovout'].numpy()
+        
+    ants.from_numpy(warped_img).to_filename(os.path.join(basedir, 'moco_manual_part1.nii'))
+
