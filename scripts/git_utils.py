e731d8d6ec31f685ed4146ddcf39dff8d811d77f
Russell Poldrack <poldrack@gmail.com>
2022-04-13 08:39:23 -0700

    reformatted using black

--- scripts/git_utils.py
+++ scripts/git_utils.py
@@ -7,4 +7,4 @@ import git
 def get_current_git_hash(return_length=8):
     script = os.path.realpath(__file__)
     repo = git.Repo(path=script, search_parent_directories=True)
-    return(repo.head.object.hexsha[:return_length])
+    return repo.head.object.hexsha[:return_length]
