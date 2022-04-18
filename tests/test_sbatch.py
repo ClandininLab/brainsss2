801102647b8bd1dc72175652e7782e32926538ab
Russ Poldrack <poldrack@gmail.com>
2022-04-18 16:34:59 -0700

    working simple tests

--- tests/test_sbatch.py
+++ tests/test_sbatch.py
@@ -4,16 +4,42 @@ import sys
 import os
 sys.path.append('../brainsss')
 sys.path.append('../brainsss/scripts')
-from slurm_utils import slurm_submit, wait_for_job
 import logging
+from slurm import SlurmBatchJob
+import pytest
 
+@pytest.fixture
+def sbatch():
+    argdict = {'foo': 1}
+    return(SlurmBatchJob('test', 'dummy_script.py', argdict))
 
-def test_sbatch():
+def test_sbatch_argdict(sbatch):
+    argdict = {'foo': 1}
+    sbatch = SlurmBatchJob('test', 'dummy_script.py', argdict)
+    assert sbatch.args['foo'] == 1
+
+def test_sbatch_run(sbatch):
+    sbatch.run()
+    assert sbatch.job_id is not None
+
+def test_sbatch_status(sbatch):
+    status = sbatch.status()
+    assert status is not None
+
+def test_sbatch_wait(sbatch):
+    sbatch.run()
+    sbatch.wait()
+    assert sbatch.status() == 'COMPLETED'
+
+def sbatch_older():
     jobname = 'test'
     script = 'sbatch.py'
     args = {'sleep': 10}
     if not os.path.exists('logs'):
         os.mkdir('logs')
+    com_dir = './com'
+    if not os.path.exists(com_dir):
+        os.mkdir(com_dir)
     logfile = 'logs/sbatch_test.log'
 
     logging_handlers = [logging.FileHandler(logfile)]
@@ -32,7 +58,7 @@ def test_sbatch():
         logging.info('job_id is None')
     else:
         logging.info(f'job_id: {job_id}')
-        output = wait_for_job(job_id, './com')
+        output = wait_for_job(job_id, com_dir)
         logging.info('completed')
         logging.info(f'output:\n{output}')
     
