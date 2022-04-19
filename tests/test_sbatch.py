f4f0a6d7b02998d4fbfa9e0cf7c6ffba1935ec64
Russell Poldrack <poldrack@gmail.com>
2022-04-19 12:23:33 -0700

    cleanup, remove older tests

--- tests/test_sbatch.py
+++ tests/test_sbatch.py
@@ -1,19 +1,20 @@
 # test basic sbatch functionality on single node host
+# pyright: reportMissingImports=false
 
 import sys
 import os
 
 sys.path.append("../brainsss")
 sys.path.append("../brainsss/scripts")
-import logging
-from slurm import SlurmBatchJob
-import pytest
+import logging  # noqa
+from slurm import SlurmBatchJob  # noqa
+import pytest  # noqa
 
 
 @pytest.fixture
 def sbatch():
     argdict = {"foo": 1}
-    return SlurmBatchJob("test", "dummy_script.py", 
+    return SlurmBatchJob("test", "dummy_script.py",
         argdict, logfile='logs/sbatch_test.log')
 
 
@@ -40,37 +41,3 @@ def test_sbatch_wait(sbatch):
     assert os.path.exists(sbatch.logfile)
     assert os.path.exists(os.path.join(sbatch.logdir, f'test_{sbatch.job_id}.out'))
     assert os.path.exists(os.path.join(sbatch.logdir, f'test_{sbatch.job_id}.stderr'))
-
-
-def sbatch_older():
-    jobname = "test"
-    script = "sbatch.py"
-    args = {"sleep": 10}
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-    com_dir = "./com"
-    if not os.path.exists(com_dir):
-        os.mkdir(com_dir)
-    logfile = "logs/sbatch_test.log"
-
-    logging_handlers = [logging.FileHandler(logfile)]
-    logging_handlers.append(logging.StreamHandler())
-
-    logging.basicConfig(
-        handlers=logging_handlers,
-        level=logging.INFO,
-        format="%(message)s",
-        datefmt="%m/%d/%Y %I:%M:%S %p",
-    )
-
-    job_id = slurm_submit(jobname, script, args, logfile)
-
-    if job_id is None:
-        logging.info("job_id is None")
-    else:
-        logging.info(f"job_id: {job_id}")
-        output = wait_for_job(job_id, com_dir)
-        logging.info("completed")
-        logging.info(f"output:\n{output}")
-
-    assert "Hello, world!" in output
