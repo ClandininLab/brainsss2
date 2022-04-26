67bf452c737ca50b37af77a3a306ea5e3235ec74
Russ Poldrack <poldrack@gmail.com>
2022-04-25 17:27:16 -0700

    new model

--- scripts/nii_to_h5.py
+++ scripts/nii_to_h5.py
@@ -4,13 +4,15 @@ import numpy as np
 import argparse
 import nibabel as nib
 import h5py
-
+from hdf5_utils import get_chunk_boundaries
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='convert nii to h5')
     parser.add_argument('-f', '--file', type=str, help='file to process', required=True)
     parser.add_argument('-o', '--outfile', type=str, help='output file')
+    parser.add_argument('--stepsize', type=int, default=50, help='stepsize for chunking')
+    parser.add_argument('--dtype', type=str, default='float32', help='dtype for output file')
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -24,7 +26,18 @@ if __name__ == '__main__':
     img = nib.load(args.file)
 
     with h5py.File(args.outfile, 'w') as f:
-        f.create_dataset('data', data=img.get_fdata(dtype='float32'), dtype="float32", chunks=True)
+        f.create_dataset('data', data=np.zeros(img.shape, dtype=args.dtype),
+                         dtype=args.dtype, 
+                         chunks=True)
         f.create_dataset('qform', data=img.header.get_qform())
         f.create_dataset('zooms', data=img.header.get_zooms())
         f.create_dataset('xyzt_units', data=img.header.get_xyzt_units())
+
+    chunk_boundaries = get_chunk_boundaries(args.stepsize, img.shape[-1])
+
+    for chunk_num, (chunk_start, chunk_end) in enumerate(chunk_boundaries):
+        print(f"Processing chunk {chunk_num + 1} of {len(chunk_boundaries)}")
+        chunk = img.get_fdata(dtype='float32')[:, :, :, chunk_start:chunk_end]
+
+        f["data"][:, :, : chunk_start:chunk_end] = chunk
+
