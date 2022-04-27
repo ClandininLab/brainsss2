dd1e750d288d58858d863cc0e23a25296b16493c
Russell Poldrack <poldrack@gmail.com>
2022-04-27 10:01:40 -0700

    move guts to separate function

--- scripts/h5_to_nii.py
+++ scripts/h5_to_nii.py
@@ -5,21 +5,8 @@ import nibabel as nib
 import h5py
 
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='convert h5 to nii')
-    parser.add_argument('-f', '--file', type=str, help='file to process', required=True)
-    parser.add_argument('-o', '--outfile', type=str, help='output file')
-
-    args = parser.parse_args(sys.argv[1:])
-
-    assert 'h5' in args.file, 'file must be h5'
-    if args.outfile is None:
-        args.outfile = args.file.replace('h5', 'nii')
-
-    print(f'converting {args.file} to {args.outfile}')
-
-    with h5py.File(args.file, 'r') as f:
+def h5_to_nii(file, outfile):
+    with h5py.File(file, 'r') as f:
         image_array = f.get("data")[:].astype('float32')
 
         if 'qform' in f:
@@ -46,8 +33,27 @@ if __name__ == '__main__':
         img.header.set_qform(qform)
         img.header.set_sform(qform)
     if zooms is not None:
-        img.header.set_zooms(zooms[:3])
+        if image_array.shape[-1] == 3:
+            img.header.set_zooms(zooms[:3])
+        else:
+            img.header.set_zooms(zooms)
     if xyzt_units is not None:
         img.header.set_xyzt_units(xyz=xyzt_units[0], t=xyzt_units[1])
 
-    img.to_filename(args.outfile)
+    img.to_filename(outfile)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='convert h5 to nii')
+    parser.add_argument('-f', '--file', type=str, help='file to process', required=True)
+    parser.add_argument('-o', '--outfile', type=str, help='output file')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    assert 'h5' in args.file, 'file must be h5'
+    if args.outfile is None:
+        args.outfile = args.file.replace('h5', 'nii')
+
+    print(f'converting {args.file} to {args.outfile}')
+    h5_to_nii(args.file, args.outfile)
