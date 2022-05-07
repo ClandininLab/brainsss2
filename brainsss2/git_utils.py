70908bf418c66b935699edb6c3f0755eafc664a9
Russell Poldrack <poldrack@gmail.com>
2022-05-07 12:24:30 -0500

    handle repo failure

--- brainsss2/git_utils.py
+++ brainsss2/git_utils.py
@@ -5,6 +5,11 @@ import git
 
 
 def get_current_git_hash(return_length=8):
-    script = os.path.realpath(__file__)
-    repo = git.Repo(path=script, search_parent_directories=True)
-    return repo.head.object.hexsha[:return_length]
+    try:
+        script = os.path.realpath(__file__)
+        repo = git.Repo(path=script, search_parent_directories=True)
+        return repo.head.object.hexsha[:return_length]
+
+    except Exception as e:
+        print(f'unable to obtain git hash: {e}')
+        return None
