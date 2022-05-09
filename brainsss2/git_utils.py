5153e9dfbb8aafb72ee926094b5ddb42812c2072
Russell Poldrack <poldrack@gmail.com>
2022-05-09 16:01:54 -0500

    add file arg

--- brainsss2/git_utils.py
+++ brainsss2/git_utils.py
@@ -4,10 +4,14 @@ import os
 import git
 
 
-def get_current_git_hash(return_length=8):
+def get_current_git_hash(file=None, return_length=8):
     try:
-        script = os.path.realpath(__file__)
+        if file is None:
+            file = __file__
+        script = os.path.realpath(file)
+        print(script)
         repo = git.Repo(path=script, search_parent_directories=True)
+        print(repo)
         return repo.head.object.hexsha[:return_length]
 
     except Exception as e:
