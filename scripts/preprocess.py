a12b48f349f1e9c58eaeb56aee212d26a1dea858
RP <poldrack@gmail.com>
2022-05-20 07:26:50 -0700

    fix filtering of func dirs

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -150,7 +150,7 @@ def run_preprocessing_step(script, args, args_dict):
     saved_handlers = []
 
     for procdir in procdirs:
-        if args.func_dirs is not None and procdir.split('/')[-1] not in args.func_dirs:
+        if args.dirtype == 'func' and args.func_dirs is not None and procdir.split('/')[-1] not in args.func_dirs:
             logging.info(f'skipping {procdir} - not included in --func_dirs')
             continue
 
