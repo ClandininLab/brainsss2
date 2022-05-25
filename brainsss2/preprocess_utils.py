34e02c565f68c899cd045d0f2b487f643b513be7
Russ Poldrack <poldrack@gmail.com>
2022-05-25 16:35:22 -0700

    rework json funcs, cleanup

--- brainsss2/preprocess_utils.py
+++ brainsss2/preprocess_utils.py
@@ -4,13 +4,15 @@ import json
 import shlex
 import subprocess
 import argparse
+import pkg_resources
 
 
 # from https://stackoverflow.com/questions/21953835/run-subprocess-and-print-output-to-logging
-def run_shell_command(command_line):
+def run_shell_command(command_line, verbose=False):
     command_line_args = shlex.split(command_line)
 
-    logging.info('Subprocess: "' + command_line + '"')
+    if verbose:
+        logging.info('Subprocess: "' + command_line + '"')
 
     try:
         command_line_process = subprocess.Popen(
@@ -21,14 +23,15 @@ def run_shell_command(command_line):
 
         process_output, _ = command_line_process.communicate()
 
-        logging.info(f'got line from subprocess: {process_output.decode("utf-8")}')
+        if verbose:
+            logging.info(f'got line from subprocess: {process_output.decode("utf-8")}')
     except Exception as e:
-        logging.info(f'Exception occured: {e}')
-        logging.info('Subprocess failed')
+        logging.error(f'Exception occured: {e}')
         return False
     else:
         # no exception was raised
-        logging.info('Subprocess finished')
+        if verbose:
+            logging.info('Subprocess finished')
 
     return process_output.decode("utf-8")
 
@@ -54,44 +57,60 @@ def dict_to_args_list(d):
             argslist.append(f"--{k}")
             argslist.extend(f"{vv}" for vv in v)
         elif v is not None:
-            pass # raise ValueError(f"unsupported type {type(v)}")
+            pass  # raise ValueError(f"unsupported type {type(v)}")
     return argslist
 
 
-def load_user_settings_from_json(args):
-    """load user settings from JSON file, overriding command line arguments
-    - first try ~/.brainsss/settings.json, then try users/<user>.json"""
+def load_default_settings_from_json(args):
+    """load default settings from the package
+    settings are structured as a dict of dicts
+    each top level dict is a preprocessing step (named as in the workflow dict in preprocess.py)
+    each next level dict contains settings for that step, with the same keys as the workflow dict
+    """
+    default_settings_file = pkg_resources.resource_filename('brainsss2', 'settings/settings.json')
+    with open(default_settings_file) as f:
+        setattr(args, 'default_settings', json.load(f))
+    logging.info("loaded default settings")
+    return args
 
-    if args.settings_file is None:
-        # first try ~/.brainsss/settings.json
+
+def load_user_settings_from_json(args, user_settings_file=None):
+    """load user settings from JSON file ( ~/.brainsss/settings.json),
+    and overwrite default settings with any user settings
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        parsed command line arguments
+    user_settings_file : str
+        path to settings file (defaults to user file)
+
+    Returns
+    -------
+    args : argparse.Namespace
+    """
+
+    assert args.default_settings is not None, "default settings just be created before adding user settings"
+    # start with default settings
+    setattr(args, 'preproc_settings', args.default_settings)
+
+    if user_settings_file is None:
         user_settings_file = os.path.join(
             os.path.expanduser("~"), ".brainsss", "settings.json"
         )
-        if os.path.exists(user_settings_file):
-            args.settings_file = user_settings_file
 
-    if args.no_require_settings and args.settings_file is None:
-        logging.info("settings file not found, using default settings from command line")
+    if not os.path.exists(user_settings_file):
+        logging.info(f"settings file {user_settings_file} not found, using package default settings")
         return args
-    elif args.settings_file is None:
-        raise FileNotFoundError('settings file not found, use --no_require_settings to use default settings from command line')
 
-    assert os.path.exists(args.settings_file), f"""
-settings file does not exist
-
-To fix this, copy {os.path.realpath(__file__)}/user.json.example to ~/.brainsss/settings.json
-and update the values
-OR turn settings file off with --no_require_settings"""
-
-    with open(args.settings_file) as f:
+    with open(user_settings_file) as f:
         user_settings = json.load(f)
-    for k, v in user_settings.items():
-        if v == "True":
-            v = True
-        elif v == "False":
-            v = False
-        setattr(args, k, v)
-    logging.info(f"loaded settings from {args.settings_file}")
+    logging.info(f"loaded user settings from {user_settings_file}")
+
+    # overwrite default settings with user settings
+    for step in user_settings:
+        for key, value in user_settings[step].items():
+            args.preproc_settings[step][key] = value
     return args
 
 
