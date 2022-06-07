fa2512d42b7e1bd6965dae9c98365955f56e5d65
Russ Poldrack <poldrack@gmail.com>
2022-06-07 16:42:34 -0700

    move make_clean_anat to module file

--- scripts/atlas_registration.py
+++ scripts/atlas_registration.py
@@ -7,16 +7,14 @@ import sys
 import ants
 import logging
 from dipy.viz import regtools
-import nibabel as nib
-import scipy.stats
 import numpy as np
-from sklearn.preprocessing import quantile_transform
 from nilearn.plotting import plot_roi, plot_anat
 
 from brainsss2.argparse_utils import get_base_parser, add_moco_arguments # noqa
 from brainsss2.logging_utils import setup_logging # noqa
 from brainsss2.imgmath import imgmath # noqa
 from brainsss2.preprocess_utils import check_for_existing_files, dump_args_to_json
+from brainsss2.atlas_registration import make_clean_anat
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 
@@ -78,43 +76,6 @@ def parse_args(input, allow_unknown=True):
         args = parser.parse_args()
 
     return args
-
-
-def make_clean_anat(anatfile, sigma=5, thresh_pct=40, normalize=False):
-    anat_img = nib.load(anatfile)
-    anat_ants = ants.image_read(anatfile)
-    low_thresh = scipy.stats.scoreatpercentile(anat_ants.numpy(), thresh_pct)
-    anat_ants_masked = ants.get_mask(anat_ants,
-        low_thresh=low_thresh,
-        cleanup=4)
-    maskfile = anatfile.replace('.nii', '_mask.nii')
-    assert maskfile != anatfile, 'maskfile should not be the same as anatfile'
-    anat_ants_masked.to_filename(maskfile)
-
-    brain = anat_img.get_fdata(dtype='float32') * anat_ants_masked[...]
-
-    # ### Blur brain and mask small values ###
-    # brain_copy = brain.copy()
-    # brain_copy = gaussian_filter(brain_copy, sigma=sigma)
-    # threshold = triangle(brain_copy)
-    # brain_copy[np.where(brain_copy < threshold/2)] = 0
-
-    # ### Remove blobs outside contiguous brain ###
-    # labels, label_nb = scipy.ndimage.label(brain_copy)
-    # brain_label = np.bincount(labels.flatten())[1:].argmax()+1
-    # brain_copy = brain.copy().astype('float32')
-    # brain_copy[np.where(labels != brain_label)] = np.nan
-
-    ### Perform quantile normalization ###
-    if normalize:
-        brain_out = quantile_transform(brain.flatten().reshape(-1, 1), n_quantiles=500, random_state=0, copy=True)
-        brain_out = brain_out.reshape(brain.shape)
-        brain = np.nan_to_num(brain_out, copy=False)
-
-    clean_anatfile = anatfile.replace('.nii', '_clean.nii')
-    assert clean_anatfile != anatfile, 'clean_anatfile is same as anatfile'
-    nib.save(nib.Nifti1Image(brain, anat_img.affine, anat_img.header), clean_anatfile)
-    return clean_anatfile, maskfile
 
 
 if __name__ == "__main__":
