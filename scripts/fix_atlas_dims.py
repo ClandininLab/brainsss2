40fbed80d77c81cd51c01406265fe3c03b0d3d05
Russell Poldrack <poldrack@gmail.com>
2022-06-15 09:36:10 -0700

    fix 2mmiso

--- scripts/fix_atlas_dims.py
+++ scripts/fix_atlas_dims.py
@@ -2,20 +2,23 @@
 import nibabel
 from pathlib import Path
 import numpy as np
+import os
 
+atlasdir = '/Users/poldrack/data_unsynced/brainsss/flydata/atlas'
 
-atlasdir = '/data/brainsss/atlas'
-
-atlasfiles = [i.as_posix() for i in Path(atlasdir).glob('*.nii')]
+atlasfiles = ['20220301_luke_2_jfrc_affine_zflip_2umiso.nii']
 
 for file in atlasfiles:
+    file = os.path.join(atlasdir, file)
     print(file)
     outfile = file.replace('.nii', '_fixed.nii')
     assert file != outfile, f'outfile should be different from file: {outfile}'
     img = nibabel.load(file)
-    print(img.header)
-    resolution = [0.00038, 0.00038, 0.00038, 1]
+    resolution = [2, 2, 2, 1]
     img.header.set_zooms(resolution[:3])
     img.header.set_xyzt_units(xyz='mm')
     img.header.set_qform(np.diag(np.array(resolution)))
-    img.to_filename(outfile)
+    img.header.set_sform(np.diag(np.array(resolution)))
+    newimg = nibabel.Nifti1Image(img.get_fdata(),
+        img.header.get_sform(), img.header)
+    newimg.to_filename(outfile)
